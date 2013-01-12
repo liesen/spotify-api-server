b401b93f19d44bca0519dbb5d50a443edaeb1ac8
Johan Liesén <johan@liesen.se>
2013-01-12 14:18:22 +0100

    Append tracks to playlist if no index argument is given

--- server.c
+++ server.c
@@ -453,9 +453,7 @@ static void put_playlist_add_tracks(sp_playlist *playlist,
   int index;
 
   if (index_field == NULL || sscanf(index_field, "%d", &index) <= 0) {
-    send_error(request, HTTP_BADREQUEST,
-               "Bad parameter: index must be numeric");
-    return;
+    index = sp_playlist_num_tracks(playlist);
   }
 
   // Parse JSON
