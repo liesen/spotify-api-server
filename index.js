aa0b1a01dc7554a82608f8c9916beece13fd25be
Johan Liesén <johan@liesen.se>
2013-04-09 19:56:54 +0200

    Use util.debug for such statements

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var ref = require('ref');
 var ffi = require('ffi');
 var libspotify = require('./lib/libspotify');
+var util = require('util');
 
 function debug() {
   if (process.env.DEBUG) {
@@ -13,7 +14,7 @@ var timeoutPtr = ref.alloc('int');
 var timeoutId;
 
 function process_events(session) {
-  debug('process_events');
+  util.debug('process_events');
   clearTimeout(timeoutId);
 
   // Very important that this uses async
@@ -35,52 +36,52 @@ var session_callbacks = new libspotify.sp_session_callbacks;
 session_callbacks['ref.buffer'].fill(0);
 
 session_callbacks.logged_in = ffi.Callback('void', [libspotify.sp_sessionPtr, 'int'], function (session, error) {
-  debug('logged_in', libspotify.CONSTANTS.sp_error[error]);
+  util.debug('logged_in: ' + libspotify.CONSTANTS.sp_error[error]);
 
   process.on('SIGINT', function () {
-    libspotify.libspotify.sp_session_logout(sessionPtr);
+    libspotify.libspotify.sp_session_logout.async(sessionPtr, function () { });
   });
 });
 
 session_callbacks.logged_out = ffi.Callback('void', [libspotify.sp_sessionPtr], function (session) {
-  debug('logged_out');
+  util.debug('logged_out');
   clearTimeout(timeoutId);
 });
 
 session_callbacks.log_message = ffi.Callback('void', [libspotify.sp_sessionPtr, 'string'], function (session, message) {
-  debug('log_message', message.trim());
+  util.debug('log_message: ' + message.trim());
 });
 
 session_callbacks.message_to_user = ffi.Callback('void', [libspotify.sp_sessionPtr, 'string'], function (session, message) {
-  debug('message_to_user', message.trim());
+  util.debug('message_to_user: ' + message.trim());
 });
 
 session_callbacks.notify_main_thread = ffi.Callback('void', [libspotify.sp_sessionPtr], function (session, error) {
-  debug('notify_main_thread');
+  util.debug('notify_main_thread');
   setImmediate(process_events, session);
 });
 
 // logged_in
 // logged_out
-session_callbacks.metadata_updated = ffi.Callback('void', [libspotify.sp_sessionPtr], function (session) { debug('metadata_updated'); });
-session_callbacks.connection_error = ffi.Callback('void', [libspotify.sp_sessionPtr, 'int'], function (session, error) { debug('connection_error'); });
+session_callbacks.metadata_updated = ffi.Callback('void', [libspotify.sp_sessionPtr], function (session) { util.debug('metadata_updated'); });
+session_callbacks.connection_error = ffi.Callback('void', [libspotify.sp_sessionPtr, 'int'], function (session, error) { util.debug('connection_error'); });
 // message_to_user
 // notify_main_thread
-session_callbacks.music_delivery = ffi.Callback('int', [libspotify.sp_sessionPtr, 'pointer', 'pointer', 'int'], function (session, audioformat, frames, numFrames) { debug('music_delivery'); });
-session_callbacks.play_token_lost = ffi.Callback('void', [libspotify.sp_sessionPtr], function (session) { debug('play_token_lost'); });
+session_callbacks.music_delivery = ffi.Callback('int', [libspotify.sp_sessionPtr, 'pointer', 'pointer', 'int'], function (session, audioformat, frames, numFrames) { util.debug('music_delivery'); });
+session_callbacks.play_token_lost = ffi.Callback('void', [libspotify.sp_sessionPtr], function (session) { util.debug('play_token_lost'); });
 // log_message
-session_callbacks.end_of_track = ffi.Callback('void', [libspotify.sp_sessionPtr], function (session) { debug('end_of_track'); });
-session_callbacks.streaming_error = ffi.Callback('void', [libspotify.sp_sessionPtr, 'int'], function (session, error) { debug('streaming_error'); });
-session_callbacks.userinfo_updated = ffi.Callback('void', [libspotify.sp_sessionPtr], function (session) { debug('userinfo_updated'); });
-session_callbacks.start_playback = ffi.Callback('void', [libspotify.sp_sessionPtr], function (session) { debug('start_playback'); });
-session_callbacks.stop_playback = ffi.Callback('void', [libspotify.sp_sessionPtr], function (session) { debug('stop_playback'); });
-session_callbacks.get_audio_buffer_stats = ffi.Callback('void', [libspotify.sp_sessionPtr, 'pointer'], function (session, stats) { debug('get_audio_buffer_stats'); });
-session_callbacks.offline_status_updated = ffi.Callback('void', [libspotify.sp_sessionPtr], function (session) { debug('offline_status_updated'); });
-session_callbacks.offline_error = ffi.Callback('void', [libspotify.sp_sessionPtr, 'int'], function (session, error) { debug('offline_error'); });
-session_callbacks.credentials_blob_updated = ffi.Callback('void', [libspotify.sp_sessionPtr, 'string'], function (session, blob) { debug('credentials_blob_updated'); });
-session_callbacks.connectionstate_updated = ffi.Callback('void', [libspotify.sp_sessionPtr], function (session) { debug('connectionstate_updated'); });
-session_callbacks.scrobble_error = ffi.Callback('void', [libspotify.sp_sessionPtr, 'int'], function (session, error) { debug('scrobble_error'); });
-session_callbacks.private_session_mode_changed = ffi.Callback('void', [libspotify.sp_sessionPtr, 'bool'], function (session, is_private) { debug('private_session_mode_changed'); });
+session_callbacks.end_of_track = ffi.Callback('void', [libspotify.sp_sessionPtr], function (session) { util.debug('end_of_track'); });
+session_callbacks.streaming_error = ffi.Callback('void', [libspotify.sp_sessionPtr, 'int'], function (session, error) { util.debug('streaming_error'); });
+session_callbacks.userinfo_updated = ffi.Callback('void', [libspotify.sp_sessionPtr], function (session) { util.debug('userinfo_updated'); });
+session_callbacks.start_playback = ffi.Callback('void', [libspotify.sp_sessionPtr], function (session) { util.debug('start_playback'); });
+session_callbacks.stop_playback = ffi.Callback('void', [libspotify.sp_sessionPtr], function (session) { util.debug('stop_playback'); });
+session_callbacks.get_audio_buffer_stats = ffi.Callback('void', [libspotify.sp_sessionPtr, 'pointer'], function (session, stats) { util.debug('get_audio_buffer_stats'); });
+session_callbacks.offline_status_updated = ffi.Callback('void', [libspotify.sp_sessionPtr], function (session) { util.debug('offline_status_updated'); });
+session_callbacks.offline_error = ffi.Callback('void', [libspotify.sp_sessionPtr, 'int'], function (session, error) { util.debug('offline_error'); });
+session_callbacks.credentials_blob_updated = ffi.Callback('void', [libspotify.sp_sessionPtr, 'string'], function (session, blob) { util.debug('credentials_blob_updated'); });
+session_callbacks.connectionstate_updated = ffi.Callback('void', [libspotify.sp_sessionPtr], function (session) { util.debug('connectionstate_updated'); });
+session_callbacks.scrobble_error = ffi.Callback('void', [libspotify.sp_sessionPtr, 'int'], function (session, error) { util.debug('scrobble_error'); });
+session_callbacks.private_session_mode_changed = ffi.Callback('void', [libspotify.sp_sessionPtr, 'bool'], function (session, is_private) { util.debug('private_session_mode_changed'); });
 
 session_config.callbacks = session_callbacks.ref();
 
@@ -94,7 +95,7 @@ require('fs').readFile(argv.A, function (err, applicationKey) {
   session_config.application_key_size = +applicationKey.length;
   var sessionPtrPtr = ref.alloc(ref.refType(libspotify.sp_sessionPtr));
   var create_error = libspotify.libspotify.sp_session_create(session_config.ref(), sessionPtrPtr);
-  debug('session_create', libspotify.CONSTANTS.sp_error[create_error]);
+  util.debug('session_create: ' + libspotify.CONSTANTS.sp_error[create_error]);
   sessionPtr = sessionPtrPtr.deref();
   var login_error = libspotify.libspotify.sp_session_login(sessionPtr, argv.u, argv.p, 0, null);
 
