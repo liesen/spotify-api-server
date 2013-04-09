e00aa71837b71265d38f0bde2020f5b5307634e4
Johan Liesén <johan@liesen.se>
2013-04-09 20:14:54 +0200

    Merge libspotify.libspotify with libspotify

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ var ffi = require('ffi');
 var libspotify = require('./lib/libspotify');
 var util = require('util');
 
-function debug() {
-  if (process.env.DEBUG) {
-    console.log.apply(this, arguments);
-  }
+for (var key in libspotify.libspotify) {
+  libspotify[key] = libspotify.libspotify[key];
 }
+
+delete libspotify.libspotify;
 
 var sessionPtr = ref.alloc(libspotify.sp_sessionPtr);
 var timeoutPtr = ref.alloc('int');
@@ -19,7 +19,7 @@ function process_events(session) {
 
   // Very important that this uses async
   do {
-    libspotify.libspotify.sp_session_process_events.async(session, timeoutPtr, function (err, error) {});
+    libspotify.sp_session_process_events.async(session, timeoutPtr, function (err, error) {});
   } while (timeoutPtr.deref() === 0);
 
   timeoutId = setTimeout(process_events, timeoutPtr.deref(), session);
@@ -39,7 +39,7 @@ session_callbacks.logged_in = ffi.Callback('void', [libspotify.sp_sessionPtr, 'i
   util.debug('logged_in: ' + libspotify.CONSTANTS.sp_error[error]);
 
   process.on('SIGINT', function () {
-    libspotify.libspotify.sp_session_logout.async(sessionPtr, function () { });
+    libspotify.sp_session_logout.async(sessionPtr, function () { });
   });
 });
 
@@ -90,18 +90,24 @@ var argv = require('optimist').
   demand(['A', 'u', 'p']).
   argv;
 
-require('fs').readFile(argv.A, function (err, applicationKey) {
-  session_config.application_key = applicationKey;
-  session_config.application_key_size = +applicationKey.length;
-  var sessionPtrPtr = ref.alloc(ref.refType(libspotify.sp_sessionPtr));
-  var create_error = libspotify.libspotify.sp_session_create(session_config.ref(), sessionPtrPtr);
-  util.debug('session_create: ' + libspotify.CONSTANTS.sp_error[create_error]);
-  sessionPtr = sessionPtrPtr.deref();
-  var login_error = libspotify.libspotify.sp_session_login(sessionPtr, argv.u, argv.p, 0, null);
+var application_key = require('fs').readFileSync(argv.A);
+session_config.application_key = application_key;
+console.log(session_config.application_key);
+console.log(session_config.application_key.length);
+session_config.application_key_size = +application_key.length;
+var sessionPtrPtr = ref.alloc(ref.refType(libspotify.sp_sessionPtr));
+var create_error = libspotify.sp_session_create(session_config.ref(), sessionPtrPtr);
+util.debug('session_create: ' + libspotify.CONSTANTS.sp_error[create_error]);
+sessionPtr = sessionPtrPtr.deref();
+var login_error = libspotify.sp_session_login(sessionPtr, argv.u, argv.p, 0, null);
 
-  if (login_error !== libspotify.CONSTANTS.sp_error.SP_ERROR_OK) {
-    throw new Error(libspotify.CONSTANTS.sp_error[login_error]);
-  }
+if (login_error !== libspotify.CONSTANTS.sp_error.SP_ERROR_OK) {
+  throw new Error(libspotify.CONSTANTS.sp_error[login_error]);
+}
+
+// Keep a reference to session_callbacks so that they won't get GC'd
+process.on('exit', function () {
+  session_callbacks;
 });
 
 // Web app
@@ -109,47 +115,46 @@ var express = require('express');
 var app = express();
 
 app.param('playlistUri', function (req, res, next, uri) {
-  var linkPtr = libspotify.libspotify.sp_link_create_from_string(uri);
+  var linkPtr = libspotify.sp_link_create_from_string(uri);
 
   if (!linkPtr) {
     return next(new Error('Unknown playlist URI: ' + uri));
   }
 
-  var playlist = libspotify.libspotify.sp_playlist_create(sessionPtr, linkPtr);
-  libspotify.libspotify.sp_link_release(linkPtr);
+  var playlist = libspotify.sp_playlist_create(sessionPtr, linkPtr);
+  libspotify.sp_link_release(linkPtr);
 
   function ok(playlist) {
     req.playlist = playlist;
     res.on('finish', function () {
-      libspotify.libspotify.sp_playlist_release(playlist);
+      libspotify.sp_playlist_release(playlist);
     });
     return next(null);
   }
 
-  if (libspotify.libspotify.sp_playlist_is_loaded(playlist)) {
+  if (libspotify.sp_playlist_is_loaded(playlist)) {
     return ok(playlist);
   }
 
   var callbacks = new libspotify.sp_playlist_callbacks;
   callbacks['ref.buffer'].fill(0);
   callbacks.playlist_state_changed = ffi.Callback('void', [libspotify.sp_playlistPtr, 'pointer'], function (playlist, userdata) {
-    if (libspotify.libspotify.sp_playlist_is_loaded(playlist)) {
-      libspotify.libspotify.sp_playlist_remove_callbacks(playlist, userdata, userdata);
+    if (libspotify.sp_playlist_is_loaded(playlist)) {
+      libspotify.sp_playlist_remove_callbacks(playlist, userdata, userdata);
       ok(playlist);
     }
   });
 
-  var error = libspotify.libspotify.sp_playlist_add_callbacks(playlist, callbacks.ref(), callbacks.ref());
+  var error = libspotify.sp_playlist_add_callbacks(playlist, callbacks.ref(), callbacks.ref());
 
   if (error !== libspotify.CONSTANTS.sp_error.SP_ERROR_OK) {
-    return next(new Error(libspotify.libspotify.sp_error_message(error)));
+    return next(new Error(libspotify.sp_error_message(error)));
   }
 });
 
 app.get('/playlists/:playlistUri', function (req, res) {
   var playlist = req.playlist;
-  var name = libspotify.libspotify.sp_playlist_name(playlist);
-  // libspotify.libspotify.sp_playlist_release(playlist);
+  var name = libspotify.sp_playlist_name(playlist);
   res.json({uri: req.params.playlistUri, name: name});
 });
 
